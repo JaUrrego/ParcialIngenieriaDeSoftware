a2d75f7574b5451c1bd06c6da9215fc5013ecd5c
JaUrrego <javub183@gmail.com>
2022-06-06 16:19:50 -0500

    proyecto parcial 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Receta } from './clases/receta';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Parcial03';
+  recetaArray: Receta[] =[
+    {nombre:"Sandwich", id:1, autor:"Camila Soto",descripcion:""}
+  ];
+  nuevaReceta: Receta = new Receta();
+  addoredit(){
+    this.nuevaReceta.id = this.recetaArray.length +1;
+    
+    this.recetaArray.push(this.nuevaReceta);
+
+    this.nuevaReceta = new Receta();
+  }
+  editar(receta: Receta){
+    this.nuevaReceta=receta;
+    
+
+  }
+  borrar(){
+    if(confirm("Esta seguro de querer eliminar esta receta?")){
+      this.recetaArray = this.recetaArray.filter(x => x != this.nuevaReceta)
+      this.nuevaReceta=new Receta();
+    }
+  }
+  
 }
---
+++ src/app/clases/receta.ts
@@ -0,0 +1,8 @@
+export class Receta{
+    public nombre! : string;
+    public autor! : string;
+    public id! : number;
+    public descripcion! : string;
+   
+
+}
